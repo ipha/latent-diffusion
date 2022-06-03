1897b5afd1b5ce49fa27a29b8b1ab513fff7c116
Mitch Bigelow <ipha00@gmail.com>
2022-06-03 08:26:15 -0400

    add support for running on cpu only

--- ldm/models/diffusion/ddim.py
+++ ldm/models/diffusion/ddim.py
@@ -17,8 +17,9 @@ class DDIMSampler(object):
 
     def register_buffer(self, name, attr):
         if type(attr) == torch.Tensor:
-            if attr.device != torch.device("cuda"):
-                attr = attr.to(torch.device("cuda"))
+            device_type = "cuda" if torch.cuda.is_available() else "cpu"
+            if attr.device != torch.device(device_type):
+                attr = attr.to(torch.device(device_type))
         setattr(self, name, attr)
 
     def make_schedule(self, ddim_num_steps, ddim_discretize="uniform", ddim_eta=0., verbose=True):
